4ed153bd41371e58cd4b229582c160ad4ae501ba
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-11-15 13:39:46 -0600

    created VPC and public subnet

--- main.tf
+++ main.tf
@@ -11,3 +11,21 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "ac-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "ac-vpc"
+  }
+}
+
+resource "aws_subnet" "ac-public" {
+  vpc_id     = aws_vpc.ac-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ac-public-subnet"
+  }
+}
