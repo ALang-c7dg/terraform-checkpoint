95654ed2f0bb4238cedfa0d7d52a77b8f2503c17
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-11-15 16:25:47 -0600

    added security rule back

--- main.tf
+++ main.tf
@@ -78,6 +78,13 @@ resource "aws_security_group" "ac-sec-group" {
   # }
 
 //deleted egress rule
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   tags = {
     Name = "ac-sec-group"
   }
