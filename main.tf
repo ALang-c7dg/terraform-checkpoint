fb2160b264579f9deac50ce3706aaeaa1d5c0016
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-11-15 13:55:52 -0600

    added route table association

--- main.tf
+++ main.tf
@@ -37,3 +37,21 @@ resource "aws_internet_gateway" "gw" {
     Name = "main"
   }
 }
+
+resource "aws_route_table" "ac-route-table" {
+  vpc_id = aws_vpc.ac-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "ac-route-table"
+  }
+}
+
+resource "aws_route_table_association" "one" {
+  subnet_id      = aws_subnet.ac-public.id
+  route_table_id = aws_route_table.ac-route-table.id
+}
